0d9eac572cb7da00288832929d1d893c7f7b52ec
unknown <ippk2016@yandex.ru git config --global user.name fenix0201git statutgit config --global user.email ippk2016@yandex.ru>
2019-03-24 20:25:12 +0300

    feat:function getLoveTrianglesCount

--- src/index.js
+++ src/index.js
@@ -3,5 +3,19 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
-};
+    let result = 0;
+    for(let index in preferences){
+        let first = preferences[index];
+        let second = preferences[first - 1];
+        let third = preferences[second - 1];
+        if(index == third - 1 && index != first-1 && index != second-1 && first-1 != second-1){
+            result++;
+            preferences[index] = undefined;
+            preferences[first - 1] = undefined;
+            preferences[second - 1] = undefined;
+        }
+    }
+    return result;
+}
+    
+    
